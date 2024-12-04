1b3031404c2e0872c01aba27de50a5239ecc7fb2
Areeba Naz <areebanaz941@gmail.com>
2024-12-04 09:52:07 -0800

    ALl done

--- client/src/components/Forms/axiosconfig.js
+++ client/src/components/Forms/axiosconfig.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000', // or your backend URL
+  baseURL: 'https://adventure-roads-backend.onrender.com', // or your backend URL
   headers: {
     'Content-Type': 'application/json'
   }
